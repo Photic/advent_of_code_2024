95abc09b6bffdab7cfabcbc6d3a4007866a281ef
Steen Rene Hansen <steen_home@hotmail.com>
2024-12-25 15:41:03 +0100

    day 8 part 2 done, part 1 was optimized

--- src/days/day8_resonant_collinearity.rs
+++ src/days/day8_resonant_collinearity.rs
@@ -13,18 +13,13 @@ pub(crate) fn day8_resonant_collinearity() {
 
     for (x, points) in current_2d_array.clone().iter().enumerate() {
         for (y, possible_node) in points.iter().enumerate() {
-            let mut found_node_value: Option<NodeValue> = None;
             if *possible_node != '.' {
-                found_node_value = Some(NodeValue {
-                    value: possible_node.clone(),
-                    cords: Cords { x, y },
-                });
-            }
-
-            if let Some(node_value) = found_node_value {
                 draw_all_possible_anti_nodes(
                     &mut current_2d_array,
-                    &node_value,
+                    &NodeValue {
+                        value: possible_node.clone(),
+                        cords: Cords { x, y },
+                    },
                     &mut anti_node_locations,
                 );
             }
@@ -39,17 +34,18 @@ pub(crate) fn day8_resonant_collinearity() {
 fn draw_all_possible_anti_nodes(
     current_2d_array: &mut Vec<Vec<char>>,
     node_value: &NodeValue,
-    anti_nodes_at_towers: &mut HashSet<Cords>,
+    anti_node_locations: &mut HashSet<Cords>,
 ) {
     for (x, layer) in current_2d_array.clone().iter().enumerate() {
         for (y, node) in layer.iter().enumerate() {
             if *node == node_value.value && (x != node_value.cords.x || y != node_value.cords.y) {
-                draw_antinode(
-                    current_2d_array,
-                    &node_value.cords,
-                    &Cords { x, y },
-                    anti_nodes_at_towers,
-                );
+                if let Some(node) = antinode_cords(&node_value.cords, &Cords { x, y }) {
+                    if let Some(row) = current_2d_array.get(node.x) {
+                        if let Some(_) = row.get(node.y) {
+                            anti_node_locations.insert(node);
+                        }
+                    }
+                }
             }
         }
     }
@@ -60,30 +56,7 @@ struct NodeValue {
     cords: Cords,
 }
 
-fn draw_antinode(
-    current_2d_array: &mut Vec<Vec<char>>,
-    node1: &Cords,
-    node2: &Cords,
-    anti_node_locations: &mut HashSet<Cords>,
-) {
-    for node in antinode_cords(node1, node2) {
-        if let Some(row) = current_2d_array.get(node.x) {
-            if let Some(_) = row.get(node.y) {
-                // if *anti_node == '.' {
-                //     *anti_node = '#';
-                // } else {
-
-                // }
-
-                anti_node_locations.insert(node);
-            }
-        }
-    }
-}
-
-fn antinode_cords(node1: &Cords, node2: &Cords) -> Vec<Cords> {
-    let mut cords = Vec::new();
-
+fn antinode_cords(node1: &Cords, node2: &Cords) -> Option<Cords> {
     if let (Some(x), Some(y)) = (
         node1
             .x
@@ -94,27 +67,88 @@ fn antinode_cords(node1: &Cords, node2: &Cords) -> Vec<Cords> {
             .checked_mul(2)
             .and_then(|val| val.checked_sub(node2.y)),
     ) {
-        cords.push(Cords { x, y });
+        return Some(Cords { x, y });
     }
 
-    if let (Some(x), Some(y)) = (
-        node2
-            .x
-            .checked_mul(2)
-            .and_then(|val| val.checked_sub(node1.x)),
-        node2
-            .y
-            .checked_mul(2)
-            .and_then(|val| val.checked_sub(node1.y)),
-    ) {
-        cords.push(Cords { x, y });
-    }
-
-    cords
+    None
 }
 
 pub(crate) fn day8_2_resonant_collinearity() {
     println!("Running day8.2 Resonant Collinearity");
 
-    let (content, timer) = start_day("./src/inputs/day8_test.txt");
+    let (content, timer) = start_day("./src/inputs/day8.txt");
+
+    let mut current_2d_array = content_to_2d_array(&content, false);
+
+    let mut anti_node_locations: HashSet<Cords> = HashSet::new();
+
+    for (x, points) in current_2d_array.clone().iter().enumerate() {
+        for (y, possible_node) in points.iter().enumerate() {
+            if *possible_node != '.' {
+                anti_node_locations.insert(Cords { x, y });
+                solve_all_possible_anti_nodes(
+                    &mut current_2d_array,
+                    &NodeValue {
+                        value: possible_node.clone(),
+                        cords: Cords { x, y },
+                    },
+                    &mut anti_node_locations,
+                );
+            }
+        }
+    }
+
+    // print_2d_array(&current_2d_array);
+
+    // draw_print_anti_nodes(anti_node_locations.clone(), current_2d_array);
+
+    end_day(&anti_node_locations.len(), &timer);
+}
+
+fn solve_all_possible_anti_nodes(
+    current_2d_array: &mut Vec<Vec<char>>,
+    node_value: &NodeValue,
+    anti_nodes_at_towers: &mut HashSet<Cords>,
+) {
+    for (x, layer) in current_2d_array.clone().iter().enumerate() {
+        for (y, node) in layer.iter().enumerate() {
+            if *node == node_value.value && (x != node_value.cords.x || y != node_value.cords.y) {
+                solve_antinode(
+                    current_2d_array,
+                    &node_value.cords,
+                    &Cords { x, y },
+                    anti_nodes_at_towers,
+                );
+            }
+        }
+    }
+}
+
+fn solve_antinode(
+    current_2d_array: &mut Vec<Vec<char>>,
+    node1: &Cords,
+    node2: &Cords,
+    anti_node_locations: &mut HashSet<Cords>,
+) {
+    if let Some(node) = antinode_cords(node1, node2) {
+        if let Some(row) = current_2d_array.get(node.x) {
+            if let Some(_) = row.get(node.y) {
+                anti_node_locations.insert(node);
+                return solve_antinode(current_2d_array, &node, node1, anti_node_locations);
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn draw_print_anti_nodes(anti_nodes: HashSet<Cords>, mut current_2d_array: Vec<Vec<char>>) {
+    for (x, row) in current_2d_array.clone().iter().enumerate() {
+        for (y, _) in row.iter().enumerate() {
+            if anti_nodes.contains(&Cords { x, y }) {
+                *current_2d_array.get_mut(x).unwrap().get_mut(y).unwrap() = '#';
+            }
+        }
+    }
+
+    print_2d_array(&current_2d_array);
 }
