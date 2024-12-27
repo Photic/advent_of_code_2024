43d36a5a57da1edf363caafa886a1551d8ff7456
Steen Rene Hansen <steen_home@hotmail.com>
2024-12-27 22:26:41 +0100

    Update day12_garden_groups.rs

--- src/days/day12_garden_groups.rs
+++ src/days/day12_garden_groups.rs
@@ -7,24 +7,24 @@ pub(crate) fn day12_garden_groups() {
 
     let (content, timer) = start_day("./src/inputs/day12.txt");
 
-    let mut node_2d_array = content_to_node_2d_array(&content);
+    let mut garden_2d_area = content_to_garden_2d_area(&content);
 
-    // print_garden_to_array(&node_2d_array);
+    // print_garden_to_array(&garden_2d_area);
 
-    let result = walk_node_2d_array(&mut node_2d_array);
+    let result = walk_the_garden(&mut garden_2d_area);
 
     end_day(&result, &timer);
 }
 
-fn walk_node_2d_array(node_2d_array: &mut Vec<Vec<Node>>) -> usize {
+fn walk_the_garden(garden_2d_area: &mut Vec<Vec<Node>>) -> usize {
     let mut result = 0;
 
-    for node_array in node_2d_array.clone() {
+    for node_array in garden_2d_area.clone() {
         for node in node_array {
-            let node_pointer = &node_2d_array[node.cord.x][node.cord.y];
+            let node_pointer = &garden_2d_area[node.cord.x][node.cord.y];
 
             if !node_pointer.visited.clone() && node_pointer.plant != '*' {
-                let garden_area = find_similiar(node.clone(), node_2d_array);
+                let garden_area = find_similiar(node.clone(), garden_2d_area);
 
                 // println!(
                 //     "Garden Area For Plant: {}, Area: {:?}",
@@ -39,21 +39,21 @@ fn walk_node_2d_array(node_2d_array: &mut Vec<Vec<Node>>) -> usize {
     result
 }
 
-fn find_similiar(start: Node, node_2d_array: &mut Vec<Vec<Node>>) -> GardenArea {
+fn find_similiar(start: Node, garden_2d_area: &mut Vec<Vec<Node>>) -> GardenArea {
     let mut queue = vec![start.clone()];
 
     let mut garden_area: GardenArea = GardenArea { area: 0, edges: 0 };
 
-    node_2d_array[start.cord.x][start.cord.y].visited = true;
+    garden_2d_area[start.cord.x][start.cord.y].visited = true;
 
     while let Some(working_node) = queue.pop() {
-        let (new_cords, edges) = get_edge(working_node.clone(), &node_2d_array);
+        let (new_cords, edges) = get_edge(working_node.clone(), &garden_2d_area);
 
         garden_area.edges += edges;
         garden_area.area += 1;
 
         for edge in new_cords {
-            let node_pointer = node_2d_array
+            let node_pointer = garden_2d_area
                 .get_mut(edge.x)
                 .unwrap()
                 .get_mut(edge.y)
@@ -69,13 +69,13 @@ fn find_similiar(start: Node, node_2d_array: &mut Vec<Vec<Node>>) -> GardenArea 
     garden_area
 }
 
-fn get_edge(start_node: Node, node_2d_array: &Vec<Vec<Node>>) -> (Vec<Cord>, usize) {
+fn get_edge(start_node: Node, garden_2d_area: &Vec<Vec<Node>>) -> (Vec<Cord>, usize) {
     let mut un_explored_nodes = vec![];
     let mut outside_edge = 4;
 
     for direction in DIRECTIONS {
         if let Some(new_cord) = new_position_in_direction(&start_node.cord, &direction) {
-            if let Some(row) = node_2d_array.get(new_cord.x) {
+            if let Some(row) = garden_2d_area.get(new_cord.x) {
                 if let Some(new_node) = row.get(new_cord.y) {
                     if new_node.plant == start_node.plant {
                         if !new_node.visited {
@@ -92,9 +92,9 @@ fn get_edge(start_node: Node, node_2d_array: &Vec<Vec<Node>>) -> (Vec<Cord>, usi
 }
 
 #[allow(dead_code)]
-fn print_garden_to_array(node_2d_array: &Vec<Vec<Node>>) {
+fn print_garden_to_array(garden_2d_area: &Vec<Vec<Node>>) {
     println!();
-    for node_array in node_2d_array {
+    for node_array in garden_2d_area {
         for node in node_array {
             print!("{}", node.to_string());
         }
@@ -122,7 +122,7 @@ struct GardenArea {
     edges: usize,
 }
 
-fn content_to_node_2d_array(input: &str) -> Vec<Vec<Node>> {
+fn content_to_garden_2d_area(input: &str) -> Vec<Vec<Node>> {
     let mut output = vec![];
 
     let mut working_vec: Vec<Node> = vec![];
@@ -149,33 +149,35 @@ fn content_to_node_2d_array(input: &str) -> Vec<Vec<Node>> {
         }
     }
 
-    let rows = output.len();
-    let cols = output[0].len();
-    let mut bordered_output = vec![
-        vec![
-            Node {
-                plant: '*',
-                cord: Cord { x: 0, y: 0 },
-                visited: false,
-                edges: 0,
-            };
-            cols + 2
-        ];
-        rows + 2
-    ];
+    output
 
-    for (x, node_array) in output.iter().enumerate() {
-        for (y, node) in node_array.iter().enumerate() {
-            bordered_output[x + 1][y + 1] = Node {
-                plant: node.plant,
-                cord: Cord { x: x + 1, y: y + 1 },
-                visited: node.visited,
-                edges: node.edges,
-            };
-        }
-    }
+    // let rows = output.len();
+    // let cols = output[0].len();
+    // let mut bordered_output = vec![
+    //     vec![
+    //         Node {
+    //             plant: '*',
+    //             cord: Cord { x: 0, y: 0 },
+    //             visited: false,
+    //             edges: 0,
+    //         };
+    //         cols + 2
+    //     ];
+    //     rows + 2
+    // ];
 
-    bordered_output
+    // for (x, node_array) in output.iter().enumerate() {
+    //     for (y, node) in node_array.iter().enumerate() {
+    //         bordered_output[x + 1][y + 1] = Node {
+    //             plant: node.plant,
+    //             cord: Cord { x: x + 1, y: y + 1 },
+    //             visited: node.visited,
+    //             edges: node.edges,
+    //         };
+    //     }
+    // }
+
+    // bordered_output
 }
 
 pub(crate) fn day12_2_garden_groups() {
@@ -183,11 +185,11 @@ pub(crate) fn day12_2_garden_groups() {
 
     let (content, timer) = start_day("./src/inputs/day12.txt");
 
-    let mut node_2d_array = content_to_node_2d_array(&content);
+    let mut garden_2d_area = content_to_garden_2d_area(&content);
 
-    // print_garden_to_array(&node_2d_array);
+    // print_garden_to_array(&garden_2d_area);
 
-    let result = walk_node_2d_array(&mut node_2d_array);
+    let result = walk_the_garden(&mut garden_2d_area);
 
     end_day(&result, &timer);
 }
